a2624049fbb3c756aab9a9ed29fa4a79a64fa1d4
zawar6006 <120322150+zawar6006@users.noreply.github.com>
2023-01-27 17:16:21 +0700

    Add files via upload

---
+++ src/components/LikeorSad.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn-group">
+    <button type="button" class="btn btn-sm btn-outline-success" @click="ilike">
+      <i class="bi bi-emoji-smile"></i>
+      {{ likes }} 
+    </button>
+    <button type="button" class="btn btn-sm btn-outline-danger" @click="isad">
+      <i class="bi bi-emoji-frown"></i>
+      {{ sads }} 
+    </button>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+let [likes, sads] = [ref(0), ref(0)];
+function ilike() {
+  return likes.value++;
+}
+function isad() {
+  return sads.value++;
+}
+</script>
+<style scoped></style>
